0902af5a64bce619425eea3c27bf2225b58026c8
Mark J. Titorenko <mark.titorenko@alces-software.com>
2018-05-17 13:27:30 +0100

    Well I flocked that up.

--- package/install.sh
+++ package/install.sh
@@ -6,9 +6,9 @@ sed -e "s,_cw_ROOT_,${cw_ROOT},g" \
     init/systemd/clusterware-flockd.service \
     > /etc/systemd/system/clusterware-flockd.service
 
-cp "${cw_ROOT}"/opt/flocked/etc/values.yml.ex "${cw_ROOT}"/opt/flocked/etc/values.yml
-chmod 0600 "${cw_ROOT}"/opt/flocked/etc/values.yml
-chmod 0600 "${cw_ROOT}"/opt/flocked/etc/config.yml
+cp "${cw_ROOT}"/opt/flockd/etc/values.yml.ex "${cw_ROOT}"/opt/flockd/etc/values.yml
+chmod 0600 "${cw_ROOT}"/opt/flockd/etc/values.yml
+chmod 0600 "${cw_ROOT}"/opt/flockd/etc/config.yml
 
 echo ':log_file: /var/log/flockd/flockd.log' >> "${cw_ROOT}"/opt/flockd/etc/config.yml
 
